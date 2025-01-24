864591b985d662084610d77e6c1d5c4b0cfacc29
Igor Tykhonchenko <igor.tykhonchenko@gmail.com>
2025-01-24 23:20:03 +0200

    Deleted loading screen, script

--- Assets/Scripts/Menu/LevelLoader.cs
+++ Assets/Scripts/Menu/LevelLoader.cs
@@ -5,24 +5,8 @@ using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
-   [SerializeField] private LoadingScreen loadingScreen;
 
     private void Awake() 
-    {
-        loadingScreen.ShowLoadingScreen();
-    }
-
-    public void LoadMenu()
-    {
-        SceneManager.LoadScene(1);
-    }
-
-    private void LoadRegistrationScreen()
-    {
-        SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings - 1);
-    }
-    
-    public void CompleteLoading()
     {
         if (string.IsNullOrEmpty(PlayerPrefs.GetString("PlayerName")))
         {
@@ -33,7 +17,14 @@ public class LevelLoader : MonoBehaviour
             LoadMenu();
         }
     }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
 
-
+    private void LoadRegistrationScreen()
+    {
+        SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings - 1);
+    }
     
 }
